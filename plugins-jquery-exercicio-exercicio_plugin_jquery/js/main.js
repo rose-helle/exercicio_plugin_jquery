690c47c883971bacc7d913b732ee234f5a2fc5cf
Rose <rose.helle@live.com>
2025-05-04 00:01:27 -0300

    Ajustes realizados: carousel, validação e máscaras

---
+++ plugins-jquery-exercicio-exercicio_plugin_jquery/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    });
+
+    $('#telefone').mask('(00) 00000-0000')
+    
+    $('#cpf').mask('000.000.000-00')
+    
+    $('#cep').mask('00000-000')
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+
+        submitHandler: function(form){
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function(){
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    })
+})
